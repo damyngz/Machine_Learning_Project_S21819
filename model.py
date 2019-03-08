05000e4d38ad5e16f19dfa0f676c39d0cde72461
(Lim) Ming Wei <edward.mingwei@gmail.com>
2019-03-08 18:53:58 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-
+from config import return_config_value
 #create config params to store network params?
 #static params kept for easy reference and editing, can be made dynamic by calling util
 
@@ -12,14 +12,21 @@ class CNN:
 		self.save_path = save_path
 		
 		#load hyperparams from config.ini
-		self.conv_windows 	= config.return_config_value('HYPER','conv_windows')
-		self.img_size 		= config.return_config_value('HYPER','img_size')
-		self.num_channels	= config.return_config_value('HYPER','num_channels')
+		self.img_size 		= return_config_value('HYPERPARMATERS','img_size')
+		self.num_channels	= return_config_value('HYPERPARMATERS','num_channels')
+		self.pooling_scheme = return_config_value('HYPERPARMATERS','pooling_scheme')
+		self.conv_windows 	= return_config_value('HYPERPARMATERS','conv_windows')
+		self.pool_windows	= return_config_value('HYPERPARMATERS','pool_windows')
 		
+		#coherence check
+		if len(self.pooling_scheme) != (len(self.pool_windows) + len(self.conv_windows)):
+			raise Exception('pooling scheme does not match pool and conv windows, check config.ini!')
+			
 		#TODO shift to config params
 		
 		#counter determining number of layers in model
-		self.layers = 0
+		self.pool_layers = 0
+		self.conv_layers = 0
 		
 		#logging
 		print('{} layers detected'.format(len(self.conv_windows)))
@@ -34,9 +41,10 @@ class CNN:
 							stride=1,
 							padding='SAME'):
 				
-				self.layers +=1
+				self.conv_layers +=1
 				stride_window = [1,stride,stride,1]
-				layer_name = 'conv_{}_{}'.format(num_filters,self.layers)
+				layer_name = 'conv_{}_{}'.format(num_filters,self.conv_layers)
+				
 				w = tf.Variable(tf.truncated_normal([window_size,window_size,num_input_filters,num_filters),
 														stddev = 1.0/np.sqrt(num_filters*window_size*window_size),
 														name = '{}_w'.format(layer_name))
@@ -46,23 +54,60 @@ class CNN:
 				conv = tf.nn.convo2d(input, 
 									filter=w,
 									strides=stride_window,
-									padding=padding) + b
+									padding=padding,
+									name = layer_name) + b
 				
 				#TODO
 				#apply normalization?
 				syn_output = activation(conv)
 				
-				return syn_output,window_size
+				return syn_output,num_filters
+				
+			def pooling(input,
+						window_size,
+						stride,
+						padding):
+						
+				self.pool_layers+=1
+				stride_window = [1,stride,stride,1]
+				layer_name = 'pool_{}_{}'.format(window_size,self.pool_layers)
+				
+				pool = tf.nn.max_pool(value = input,
+										ksize = window_size,
+										strides = stride_window,
+										padding = padding,
+										name = layer_name)
+										
+				return pool
+				
+				
 			
 			with self.graph_as_default():
 				input_vectors = [0 for i in range(len(conv_windows))]
 				input_vectors[0] = self.input
 				
 				self.image = tf.placeholder(tf.float32,[self.img_size*self.img_size*self.num_channels]
-				for l in range(len(conv_windows)):
+				conv_iter,pool_iter = 0,0
+				for l in range(len(self.pooling_scheme)):
 					if input_vector is None:
 						input_vector = self.input_image
 						prev_num_filters = self.num_channels
+						
+					if pooling_scheme[l] == 1:
+						#TODO add pooling layer function
+						pool_iter+=1
+						pass
+					elif pooling_scheme[l] == 0:
+						input_vector,prev_num_filters = convolution(input = input_vector,
+																	num_input_filters = prev_num_filters,
+																	window_size = self.conv_window[conv_iter],
+																	activation = tf.nn.relu,
+																	stride = 2,
+																	padding = "VALID")
+									
+						conv_iter+=1
+					else:
+						raise RuntimeError('pooling scheme value {} at layer {] is not valid'.format(pooling_scheme[l],l))
 					
 					input_vector,prev_num_filters = convolution(input = input_vector,num_input_filters = prev_num_filters, window_size = conv_windows[l],activation = (tf.nn.relu),stride = 2, padding = 'VALID')
 					
